eb333733908949f4e3b9f923b2ca2ae686f02b74
Nadegeida <idatchatchau@gmail.com>
2026-08-20 05:02:44 +0100

    Fixed favicon path and closing tag

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World! I am an alert box!");
+function My_first_Function(){
+        var string= "kiss me, I am Irish";
+        var result= string.fontcolor("green");
+        document.getElementById ("Irish").innerHTML=
+        result;
+    }
